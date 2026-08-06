2b2be43d7d9b1ff586902d65216a9bc3c370c774
Aluno SENAI <Aluno@SN-351173.edu.fiesc.com.br>
2026-08-06 20:28:55 -0300

    Sabooor tarefa 1

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
 // para "Texto alterado com sucesso!"
 //
 // Dica: use document.getElementById() e .textContent
+document.getElementById("btn1").addEventListener("click", function(){
+    document.getElementById("texto1").textContent = "Texto alterado com sucesso!"
+})
 
 
 
