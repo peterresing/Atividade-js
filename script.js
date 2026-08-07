2dd5b639796108caa28602c82546b50c122d4d3b
Aluno SENAI <Aluno@SN-351173.edu.fiesc.com.br>
2026-08-06 21:06:55 -0300

    atividade cinco

--- script.js
+++ script.js
@@ -46,7 +46,9 @@ document.getElementById("btn3").addEventListener("click", function(){
 // para amarelo
 //
 // Dica: use .style.backgroundColor = "yellow"
-
+document.getElementById("btn5").addEventListener("click", function(){
+    document.getElementById("caixa5").style.backgroundColor = "yellow"
+})
 
 
 // ---------- Exercicio 6 ----------
